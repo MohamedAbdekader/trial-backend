4a330d591e569766d3a2e0cf8bc680ec343e4c0b
James Kim <juleskondo@gmail.com>
2021-09-30 17:07:58 +0900

    try fix admin media

--- core/settings.py
+++ core/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+ADMIN_MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ADMIN_MEDIA_PREFIX = '/admin-media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
