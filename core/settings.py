0c58a8afc980f03a11b6f0b95ed0730fb90f16b2
James Kim <juleskondo@gmail.com>
2021-09-30 16:55:38 +0900

    fix allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['lime-market-backend.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['lime-market-backend.herokuapp.com', 'localhost']
 
 
 # Application definition
