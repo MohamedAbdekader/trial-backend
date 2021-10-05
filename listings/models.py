8371a6af60af7a71da867b3b9b36b469880ba876
James Kim <juleskondo@gmail.com>
2021-10-05 16:02:13 +0900

    migrations

--- listings/models.py
+++ listings/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from profiles.models import Profile
+from django.conf import settings
 
 class Listing(models.Model):
     title = models.CharField(max_length=25)
@@ -9,7 +9,7 @@ class Listing(models.Model):
     image2 = models.ImageField(blank=True)
     image3 = models.ImageField(blank=True)
     image4 = models.ImageField(blank=True)
-    creator = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     creator_img = models.ImageField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     likers = models.JSONField(default=dict)
--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-from core.listings.models import Listing
+from listings.models import Listing
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.conf import settings
