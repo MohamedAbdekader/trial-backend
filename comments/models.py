dec4a9c4a77b1dfa5b01b1417a6902639922f670
James Kim <juleskondo@gmail.com>
2021-11-02 16:22:05 +0900

    creator_img field model comments

--- comments/models.py
+++ comments/models.py
@@ -4,7 +4,7 @@ from listings.models import Listing
 
 class Comment(models.Model):
     creator = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    # creatorImg = models.ImageField(blank=True, null=True)
+    creator_img = models.CharField(max_length=500, blank=True, null=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     content = models.TextField(max_length=100, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
