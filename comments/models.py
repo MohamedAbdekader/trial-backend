3759aa2e7d5df5ef3e992d4d428c7800351301ac
James Kim <juleskondo@gmail.com>
2021-11-03 10:37:12 +0900

    profile_img default to amazon s3

--- comments/models.py
+++ comments/models.py
@@ -6,6 +6,6 @@ class Comment(models.Model):
     creator = models.ForeignKey(Profile, on_delete=models.CASCADE)
     creator_img = models.CharField(max_length=500, blank=True, null=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
-    content = models.TextField(max_length=100, blank=True, null=True)
+    content = models.TextField(max_length=5000, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(auto_now=True)
