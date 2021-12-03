e8961e46558ef99fb30d7dc99b24ae193c131c2a
James Kim <juleskondo@gmail.com>
2021-12-03 16:49:27 +0900

    cors whitelist

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,7 @@ ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = 'http://localhost:3000',
+CORS_ORIGIN_WHITELIST = 'https://lime-market.web.app/',
 
 AUTH_USER_MODEL = "profiles.Profile"
 
